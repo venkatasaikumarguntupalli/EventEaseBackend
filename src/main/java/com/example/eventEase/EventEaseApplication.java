6772acd725362c2f57b876a80cda15fa0a6279fc
Venkata Sai Kumar Guntupalli <venkataguntupalli@Venkatas-MacBook-Pro.attlocal.net>
2024-12-21 13:37:52 -0800

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/example/eventEase/EventEaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventEaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eventEase/EventEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
